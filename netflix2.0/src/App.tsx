3699ead122b81c8515fabc8064500a24178f1bb6
Biraj Shrestha <biraj9731@gmail.com>
2025-08-27 21:12:19 +0545

    last

--- netflix2.0/src/App.tsx
+++ netflix2.0/src/App.tsx
@@ -29,6 +29,13 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
      <p>qweweqwqeqhgasdhaskdajkashdjadqiwyeuiqhuisdasdjakjdjkhkj</p>
+       <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
